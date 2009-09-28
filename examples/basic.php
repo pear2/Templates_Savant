211d92692840044d8e1a27118337b925af8ab9cb
Brett Bieber <brett.bieber@gmail.com>
2009-09-28 14:06:49 +0000

    Update example to use the render method, with echo.

--- examples/basic.php
+++ examples/basic.php
@@ -10,36 +10,36 @@ $class->var1 = '<p>This is var1 inside a standard class</p>';
 $savant = new \pear2\Templates\Savant\Main();
 $savant->addPath('template','templates');
 // Display a simple string
-$savant->display('<h1>Welcome to the Savant Demo</h1>');
+echo $savant->render('<h1>Welcome to the Savant Demo</h1>');
 
 // Display a string, in a custom template
-$savant->display('mystring', 'StringView.tpl.php');
+echo $savant->render('mystring', 'StringView.tpl.php');
 
 // Display an array
-$savant->display(array('<ul>', '<li>This is an array</li>', '</ul>'));
+echo $savant->render(array('<ul>', '<li>This is an array</li>', '</ul>'));
 
 // Display an object using a default class name to template mapping function
-$savant->display($class);
+echo $savant->render($class);
 
 // Display the object using a specific template
-$savant->display($class, 'MyTemplate.tpl.php');
+echo $savant->render($class, 'MyTemplate.tpl.php');
 
-$savant->display('<h2>Output Filtering</h2>');
+$savant->render('<h2>Output Filtering</h2>');
 $savant->addFilters('htmlspecialchars');
 
 // Now show an entire template with htmlspecialchars
-$savant->display($class);
+echo $savant->render($class);
 
 // Ok, now remove the output filters
 $savant->setFilters();
 
-$savant->display('<h2>Variable Escaping</h2>');
+echo $savant->render('<h2>Variable Escaping</h2>');
 
 // Turn on some template variable escaping
 $savant->addEscape('htmlspecialchars');
 
 // Display the standard class, now with all variables accessed being escaped
-$savant->display($class);
+echo $savant->render($class);
 
 highlight_file(__FILE__);
 
